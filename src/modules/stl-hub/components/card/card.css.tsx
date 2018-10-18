9497d4b4b026351c379343d757009709c6fdcff2
topstar <topstar.superman@gmail.com>
2018-10-18 16:35:02 +0300

    Add initial card UI

---
+++ src/modules/stl-hub/components/card/card.css.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Button } from 'antd';
+
+// @ts-ignore
+export const ButtonItem = styled(Button)`
+  width: 100%;
+`;
--- src/pages/stl-hub.tsx
+++ src/pages/stl-hub.tsx
@@ -3,8 +3,10 @@ import { SFC } from 'react';
 import { ApiKeysContainer } from '../modules/api-keys/containers/api-keys/api-keys.container';
 import { MainLayout } from '../components/layouts/main-layout';
 import { StlSidebarNavigation } from '../modules/stl-hub/components/sidebar/sidebar';
+import { StlCard } from '../modules/stl-hub/components/card/card';
 
 export const StlHubLayout: SFC = () => (
   <MainLayout sidebar={<StlSidebarNavigation />}>
+    <StlCard />
   </MainLayout>
 );
