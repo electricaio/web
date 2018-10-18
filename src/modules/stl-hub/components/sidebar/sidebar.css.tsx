c105e684577123215479ac59e6cc037a2cd87905
topstar <topstar.superman@gmail.com>
2018-10-18 11:53:54 +0300

    Add STL Hub Page Sidebar

---
+++ src/modules/stl-hub/components/sidebar/sidebar.css.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Checkbox } from 'antd';
+
+export const CheckboxItem = styled(Checkbox)`
+  font-size: 15px;
+`;
---
+++ src/modules/stl-hub/components/sidebar/sidebar.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { SFC } from 'react';
+import { Navigation } from '../../../../components/navigation';
+import { CheckboxItem } from './sidebar.css';
+import { Menu, Dropdown, Icon } from 'antd';
+
+const menu = <Menu>
+  <Menu.Item key="1">Credit Card</Menu.Item>
+  <Menu.Item key="2">Paypal</Menu.Item>
+  <Menu.Item key="3">Stripe</Menu.Item>
+</Menu>;
+
+const items = [
+  {
+    action: <CheckboxItem>All</CheckboxItem>,
+  },
+  {
+    action: <CheckboxItem>Foundation</CheckboxItem>,
+  },
+  {
+    action: <Dropdown overlay={menu} trigger={['click']}>
+              <span style={{ userSelect: 'none' }}>
+                <Icon type="down" theme="outlined" /> Payment
+              </span>
+            </Dropdown>,
+  },
+  {
+    action: <CheckboxItem>CRM</CheckboxItem>,
+  },
+  {
+    action: <CheckboxItem>Support</CheckboxItem>,
+  },
+];
+
+export const StlSidebarNavigation: SFC = () => <Navigation mode="vertical" items={items} />;
