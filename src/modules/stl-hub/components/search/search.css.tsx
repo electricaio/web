86a53190326b40701e2fa295fc7a217007f05ac5
topstar <topstar.superman@gmail.com>
2018-10-19 19:32:49 +0300

    update SearchContainer with ant-d Card component.

--- src/modules/stl-hub/components/search/search.css.tsx
+++ src/modules/stl-hub/components/search/search.css.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Card } from 'antd';
 
-export const SearchContainer = styled.div`
+export const SearchContainer = styled(Card)`
   width: 80%;
-  margin: 20px auto 40px auto;
+  margin: 20px auto 40px auto !important;
+  border-radius: 5px;
+  box-shadow: 0 0 25px 0 rgba(113, 150, 193, 0.2);
 `;
