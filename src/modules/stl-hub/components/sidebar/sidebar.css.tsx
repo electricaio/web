da6daf686c749dc5506f9c8b643a733caf30977d
topstar <topstar.superman@gmail.com>
2018-10-22 04:36:44 +0300

    add custom switch styled component

--- src/modules/stl-hub/components/sidebar/sidebar.css.tsx
+++ src/modules/stl-hub/components/sidebar/sidebar.css.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Checkbox } from 'antd';
+import { Checkbox, Switch } from 'antd';
 
 export const CheckboxItem = styled(Checkbox)`
   font-size: 15px;
+`;
+
+// @ts-ignore
+export const SwitchItem = styled(Switch)`
+  background-color: #2c3847 !important;
 `;
