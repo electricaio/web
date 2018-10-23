06b9e34ee89e278748ebc2435a10b898c923670f
topstar <topstar.superman@gmail.com>
2018-10-23 22:35:07 +0300

    update default export fir stl-hub

--- src/modules/stl-hub/components/stl-hub/stl-hub.tsx
+++ src/modules/stl-hub/components/stl-hub/stl-hub.tsx
@@ -1,10 +1,10 @@
 import React, { SFC } from 'react';
 import { Row, Col } from 'antd';
 import { StlHubContainer } from './stl-hub.css';
-import { StlCard } from '../card/card';
+import StlCard from '../card/card';
 import { SearchItem } from '../search/search';
 
-export const StlPage: SFC = () => (
+const StlPage: SFC = () => (
   <StlHubContainer>
       <SearchItem/>
       <Row gutter={24}>
@@ -19,3 +19,5 @@ export const StlPage: SFC = () => (
       </Row>
   </StlHubContainer>
 );
+
+export default StlPage;
--- src/pages/stl-hub.tsx
+++ src/pages/stl-hub.tsx
@@ -3,7 +3,7 @@ import { SFC } from 'react';
 import { ApiKeysContainer } from '../modules/api-keys/containers/api-keys/api-keys.container';
 import { MainLayout } from '../components/layouts/main-layout';
 import { StlSidebarNavigation } from '../modules/stl-hub/components/sidebar/sidebar';
-import { StlPage } from '../modules/stl-hub/components/stl-hub/stl-hub';
+import StlPage from '../modules/stl-hub/components/stl-hub/stl-hub';
 
 export const StlHubLayout: SFC = () => (
   <MainLayout sidebar={<StlSidebarNavigation />}>
