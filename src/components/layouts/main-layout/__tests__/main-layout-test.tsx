1ef7d104d40ed978b58b494f8d447697e362f82f
topstar <topstar.superman@gmail.com>
2018-10-23 13:36:42 +0300

    update main-layout-test

--- src/components/layouts/main-layout/__tests__/main-layout-test.tsx
+++ src/components/layouts/main-layout/__tests__/main-layout-test.tsx
@@ -3,7 +3,9 @@ import { Layout } from 'antd';
 import { shallow } from 'enzyme';
 import { Navigation } from '../../../navigation';
 import { MainLayout } from '..';
-const { Header, Sider, Content } = Layout;
+import { LayoutHeader, LayoutSider } from '../main-layout.css';
+
+const { Content } = Layout;
 
 describe('Layout with Sidebar', () => {
   const TestContentComponent = () => <div>content</div>;
@@ -18,15 +20,15 @@ describe('Layout with Sidebar', () => {
   });
 
   it('contains a header', () => {
-    expect(this.layoutSidebar.find(Header)).toHaveLength(1);
+    expect(this.layoutSidebar.find(LayoutHeader)).toHaveLength(1);
   });
 
   it('contains a Sidebar component', () => {
-    expect(this.layoutSidebar.find(Sider)).toHaveLength(1);
+    expect(this.layoutSidebar.find(LayoutSider)).toHaveLength(1);
   });
 
   it('creates a Sidebar component from a sidebar prop', () => {
-    expect(this.layoutSidebar.find(Sider).find(TestSidebarComponent)).toHaveLength(1);
+    expect(this.layoutSidebar.find(LayoutSider).find(TestSidebarComponent)).toHaveLength(1);
   });
 
   it('contains a Content component', () => {
@@ -39,16 +41,16 @@ describe('Layout with Sidebar', () => {
 
   describe('Header', () => {
     it('has navigation in the header', () => {
-      expect(this.layoutSidebar.find(Header).find(Navigation)).toHaveLength(1);
+      expect(this.layoutSidebar.find(LayoutHeader).find(Navigation)).toHaveLength(1);
     });
 
     it('contains a list of navigation items to render', () => {
-      const header = this.layoutSidebar.find(Header).find(Navigation);
+      const header = this.layoutSidebar.find(LayoutHeader).find(Navigation);
       expect(header.prop('items').length).toBeGreaterThan(0);
     });
 
     it('contains the logo', () => {
-      expect(this.layoutSidebar.find(Header).find('.logo')).toHaveLength(1);
+      expect(this.layoutSidebar.find(LayoutHeader).find('.logo')).toHaveLength(1);
     });
   });
 });
