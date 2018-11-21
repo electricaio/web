d49004d9d91a2ef9b880f3d4fbb20dbc1678adba
Rene <rene.rondon@electrica.io>
2018-11-20 23:27:37 -0400

    wrap SignupFormComponent to redux store.

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,12 +1,12 @@
 import React, { SFC } from 'react';
 import { LayoutSkewedBackground } from '../components/layouts/layout-skewed-background';
-import { SignupFormComponent } from '../modules/signup';
+import { SignupContainer } from '../modules/signup/container/signup.container';
 import { StyledHorizonalCenteredCard } from './pages.css';
 
 export const SignupPage: SFC = () => (
   <LayoutSkewedBackground>
     <StyledHorizonalCenteredCard>
-      <SignupFormComponent />
+      <SignupContainer />
     </StyledHorizonalCenteredCard>
   </LayoutSkewedBackground>
 );
