b4e4380e5781d570cd96831aceab9a614cdf2ad3
Rene <rene.rondon@electrica.io>
2018-11-26 15:13:53 -0400

    fetch connectors from the API.

--- src/redux/connector-hub/reducer.ts
+++ src/redux/connector-hub/reducer.ts
@@ -2,12 +2,11 @@ import { Reducer } from 'redux';
 import { ConnectorHubState } from './types';
 import { ActionType, getType } from 'typesafe-actions';
 import * as actions from './actions';
-import { CONNECTOR_HUB_DATA } from '../../fixtures/connector-hub-data';
 
 export type ConnectorHubActionType = ActionType<typeof actions>;
 
 const initialState: ConnectorHubState = {
-  data: CONNECTOR_HUB_DATA,
+  data: [],
   loading: false,
 };
 
