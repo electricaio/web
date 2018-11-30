3c7c5109d4e9b076f2f15f2718759e353c062364
Rene <rene.rondon@electrica.io>
2018-11-30 05:43:47 -0400

    modal testing init.

---
+++ src/modules/connections/components/modal-button-action/__tests__/modal-button-action.test.tsx
@@ -0,0 +1,87 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import { ButtonActionModal } from '../modal-button-action';
+import { MainModal } from '../../../../../components/modal';
+import { ConnectionForm } from '../../connection-form/connection-form';
+
+describe('New Key Modal', () => {
+  beforeEach(() => {
+    this.newKeyComponent = shallow(
+      <ButtonActionModal submitText="" title="" onCommit={() => {}}>
+        <span className="clickable">Testing</span>
+      </ButtonActionModal>
+    );
+  });
+
+  const formFields = {
+    connectionName: 'test',
+  };
+
+  const mockFormRef = ({ resetFields = jest.fn(), validateFields = jest.fn() }) => {
+    return {
+      props: {
+        form: {
+          resetFields,
+          validateFields,
+        },
+      },
+    };
+  };
+
+  it('hides modal when cancel is called', () => {
+    this.newKeyComponent.setState({
+      visible: true,
+    });
+    this.newKeyComponent.instance().formRef = mockFormRef({});
+    const modal = this.newKeyComponent.find(MainModal);
+
+    expect(modal.prop('visible')).toBeTruthy();
+    modal.prop('handleCancel')();
+    expect(this.newKeyComponent.find(MainModal).prop('visible')).toBeFalsy();
+  });
+
+  it('adds onClick to child and displays modal when clicked', () => {
+    this.newKeyComponent.find('.clickable').simulate('click');
+    const modal = this.newKeyComponent.find(MainModal);
+    expect(modal.prop('visible')).toBeTruthy();
+  });
+
+  it('validates fields on save', () => {
+    const validateFieldsMock = jest.fn();
+    this.newKeyComponent.instance().formRef = mockFormRef({ validateFields: validateFieldsMock });
+    const modal = this.newKeyComponent.find(MainModal);
+    modal.prop('handleSave')();
+    expect(validateFieldsMock).toBeCalled();
+  });
+
+  it('resets fields on after validation', () => {
+    const resetFieldsMock = jest.fn();
+    const formFields = {
+      connectionName: 'test',
+    };
+    this.newKeyComponent.instance().formRef = mockFormRef({
+      validateFields: jest.fn(cb => cb(null, formFields)),
+      resetFields: resetFieldsMock,
+    });
+    const modal = this.newKeyComponent.find(MainModal);
+    modal.prop('handleSave')();
+    expect(resetFieldsMock).toBeCalled();
+  });
+
+  it('hides the modal after creation', () => {
+    this.newKeyComponent.setState({
+      visible: true,
+    });
+    this.newKeyComponent.instance().formRef = mockFormRef({
+      validateFields: jest.fn(cb => cb(null, formFields)),
+    });
+    const modal = this.newKeyComponent.find(MainModal);
+    modal.prop('handleSave')();
+    expect(this.newKeyComponent.find(MainModal).prop('visible')).toBeFalsy();
+  });
+
+  it('renders ConnectionForm', () => {
+    const connectionForm = this.newKeyComponent.find(ConnectionForm);
+    expect(connectionForm).toHaveLength(1);
+  });
+});
